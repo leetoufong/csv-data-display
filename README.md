63b90c17d5d3dec7c34e83e12ab931551cadb221
Lee <pla9@cdc.gov>
2024-02-01 17:29:05 -0500

    Update readme. Slight clean up

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CSV Data Display
 
 As the title says, fetch a CSV file, parse it...AAAAaaaand display it on a page. This react project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). Enjoy!
+
+## Technical Notes
+
+- Only takes CSV files
+- Able to check between comma or tab delimited files
--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
             try {
                 const response = await fetch('./sample.csv');
                 const text = await response.text();
-                console.log(text)
                 const json = await handleCSVToJSON(text);
 
                 setData(json);
@@ -29,7 +28,7 @@ function App() {
                 setHeadings(newHeadings);
             }
             catch(error) {
-
+                // handle errors
             }
             finally {
                 setLoading(false);
@@ -39,8 +38,18 @@ function App() {
         fetchCSV();
     }, []);
 
-    const handleCSVToJSON = (data, delimiter = ',') => {
-        const titles = data.slice(0, data.indexOf('\n')).split(delimiter);
+    const handleCSVToJSON = (data) => {
+        let titles = data.slice(0, data.indexOf('\n'));
+        let delimiter;
+
+        if (titles.indexOf(',') >= 0) {
+            delimiter = ',';
+        } else {
+            delimiter = /[ ]+/;
+        }
+
+        titles = titles.split(delimiter);
+
         return data
             .slice(data.indexOf('\n') + 1)
             .split('\n')
@@ -93,16 +102,16 @@ function App() {
                     <table className="border-collapse w-full">
                         <thead className="text-left">
                             <tr>
-                                {headings.map((item, index) => (
-                                    <th className="border p-2 font-bold bg-blue-200" key={index}>{item} <button onClick={() => handleSort(item)}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 322 511.21"><path fill-rule="nonzero" d="M295.27 211.54H26.71c-6.23-.02-12.48-2.18-17.54-6.58-11.12-9.69-12.29-26.57-2.61-37.69L144.3 9.16c.95-1.07 1.99-2.1 3.13-3.03 11.36-9.4 28.19-7.81 37.58 3.55l129.97 157.07a26.65 26.65 0 0 1 7.02 18.06c0 14.76-11.97 26.73-26.73 26.73zM26.71 299.68l268.56-.01c14.76 0 26.73 11.97 26.73 26.73 0 6.96-2.66 13.3-7.02 18.06L185.01 501.53c-9.39 11.36-26.22 12.95-37.58 3.55-1.14-.93-2.18-1.96-3.13-3.03L6.56 343.94c-9.68-11.12-8.51-28 2.61-37.69 5.06-4.4 11.31-6.56 17.54-6.57z"/></svg></button></th>
+                                {headings.map((item, i) => (
+                                    <th className="border p-2 font-bold bg-blue-200" key={i}>{item} <button onClick={() => handleSort(item)}><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 322 511.21"><path fillRule="nonzero" d="M295.27 211.54H26.71c-6.23-.02-12.48-2.18-17.54-6.58-11.12-9.69-12.29-26.57-2.61-37.69L144.3 9.16c.95-1.07 1.99-2.1 3.13-3.03 11.36-9.4 28.19-7.81 37.58 3.55l129.97 157.07a26.65 26.65 0 0 1 7.02 18.06c0 14.76-11.97 26.73-26.73 26.73zM26.71 299.68l268.56-.01c14.76 0 26.73 11.97 26.73 26.73 0 6.96-2.66 13.3-7.02 18.06L185.01 501.53c-9.39 11.36-26.22 12.95-37.58 3.55-1.14-.93-2.18-1.96-3.13-3.03L6.56 343.94c-9.68-11.12-8.51-28 2.61-37.69 5.06-4.4 11.31-6.56 17.54-6.57z"/></svg></button></th>
                                 ))}
                             </tr>
                         </thead>
                         <tbody>
                             {data.map((item, i) => (
                                 <tr className="hover:bg-slate-100" key={i}>
-                                    {Object.keys(item).map((datum) => (
-                                        <td className="border p-2">{item[datum]}</td>
+                                    {Object.keys(item).map((datum, j) => (
+                                        <td className="border p-2" key={j}>{item[datum]}</td>
                                     ))}
                                 </tr>
                             ))}
